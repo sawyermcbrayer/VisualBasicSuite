a3b57d23441ab6c081e9567242bc774007593757
sawyermcbrayer <90974302+sawyermcbrayer@users.noreply.github.com>
2022-02-18 17:03:19 +0200

    Add files via upload

---
+++ UserInputForm/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnInput = New System.Windows.Forms.Button()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.lblFirstName = New System.Windows.Forms.Label()
+        Me.lblLastName = New System.Windows.Forms.Label()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtGender = New System.Windows.Forms.TextBox()
+        Me.lblGender = New System.Windows.Forms.Label()
+        Me.lstOccupation = New System.Windows.Forms.ListBox()
+        Me.lblOccupation = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnInput
+        '
+        Me.btnInput.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.btnInput.Location = New System.Drawing.Point(300, 324)
+        Me.btnInput.Name = "btnInput"
+        Me.btnInput.Size = New System.Drawing.Size(157, 33)
+        Me.btnInput.TabIndex = 0
+        Me.btnInput.Text = "Click Me!"
+        Me.btnInput.UseVisualStyleBackColor = False
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(300, 136)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(152, 20)
+        Me.txtFirstName.TabIndex = 1
+        '
+        'lblFirstName
+        '
+        Me.lblFirstName.AutoSize = True
+        Me.lblFirstName.Location = New System.Drawing.Point(238, 139)
+        Me.lblFirstName.Name = "lblFirstName"
+        Me.lblFirstName.Size = New System.Drawing.Size(60, 13)
+        Me.lblFirstName.TabIndex = 2
+        Me.lblFirstName.Text = "First Name:"
+        '
+        'lblLastName
+        '
+        Me.lblLastName.AutoSize = True
+        Me.lblLastName.Location = New System.Drawing.Point(237, 169)
+        Me.lblLastName.Name = "lblLastName"
+        Me.lblLastName.Size = New System.Drawing.Size(61, 13)
+        Me.lblLastName.TabIndex = 4
+        Me.lblLastName.Text = "Last Name:"
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(300, 166)
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(152, 20)
+        Me.txtLastName.TabIndex = 5
+        '
+        'txtGender
+        '
+        Me.txtGender.Location = New System.Drawing.Point(300, 196)
+        Me.txtGender.Name = "txtGender"
+        Me.txtGender.Size = New System.Drawing.Size(152, 20)
+        Me.txtGender.TabIndex = 6
+        '
+        'lblGender
+        '
+        Me.lblGender.AutoSize = True
+        Me.lblGender.Location = New System.Drawing.Point(253, 199)
+        Me.lblGender.Name = "lblGender"
+        Me.lblGender.Size = New System.Drawing.Size(45, 13)
+        Me.lblGender.TabIndex = 7
+        Me.lblGender.Text = "Gender:"
+        '
+        'lstOccupation
+        '
+        Me.lstOccupation.FormattingEnabled = True
+        Me.lstOccupation.Items.AddRange(New Object() {"Automotive Technician", "Irrigation Specialist", "Lift Operator", "Software Developer"})
+        Me.lstOccupation.Location = New System.Drawing.Point(300, 223)
+        Me.lstOccupation.Name = "lstOccupation"
+        Me.lstOccupation.Size = New System.Drawing.Size(152, 43)
+        Me.lstOccupation.TabIndex = 8
+        '
+        'lblOccupation
+        '
+        Me.lblOccupation.AutoSize = True
+        Me.lblOccupation.Location = New System.Drawing.Point(233, 223)
+        Me.lblOccupation.Name = "lblOccupation"
+        Me.lblOccupation.Size = New System.Drawing.Size(65, 13)
+        Me.lblOccupation.TabIndex = 9
+        Me.lblOccupation.Text = "Occupation:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblOccupation)
+        Me.Controls.Add(Me.lstOccupation)
+        Me.Controls.Add(Me.lblGender)
+        Me.Controls.Add(Me.txtGender)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.lblLastName)
+        Me.Controls.Add(Me.lblFirstName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Controls.Add(Me.btnInput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnInput As Button
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents lblFirstName As Label
+    Friend WithEvents lblLastName As Label
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents txtGender As TextBox
+    Friend WithEvents lblGender As Label
+    Friend WithEvents lstOccupation As ListBox
+    Friend WithEvents lblOccupation As Label
+End Class
---
+++ UserInputForm/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+
+    Private Sub btnInput_Click(sender As Object, e As EventArgs) Handles btnInput.Click
+
+        Dim stFirst As String
+        Dim stLast As String
+        Dim stGender As String
+        Dim stOccupation As String
+
+
+        stFirst = txtFirstName.Text
+        stLast = txtLastName.Text
+        stGender = txtGender.Text
+        stOccupation = lstOccupation.SelectedItem
+
+        MessageBox.Show("First Name: " & stFirst & vbNewLine _
+                        & "Last Name: " & stLast & vbNewLine _
+                        & "Gender: " & stGender & vbNewLine _
+                        & "Your occupation is: " & stOccupation)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        lstOccupation.Items.Add("Traveler")
+        lstOccupation.Items.Add("Foodie")
+        lstOccupation.Items.Add("Snow Boarder")
+        lstOccupation.Items.Add("Rock Climber")
+
+    End Sub
+End Class
