ebbf3d560e1b50aebf003ed6097b71af4447bb94
sawyermcbrayer <90974302+sawyermcbrayer@users.noreply.github.com>
2022-02-19 02:46:41 +0200

    Second Wave!(2)

---
+++ BasicSearches/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnLinSearch = New System.Windows.Forms.Button()
+        Me.btn2DArray = New System.Windows.Forms.Button()
+        Me.btnRow = New System.Windows.Forms.Button()
+        Me.btnColumn = New System.Windows.Forms.Button()
+        Me.btnFullArray = New System.Windows.Forms.Button()
+        Me.btnSearch2D = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnLinSearch
+        '
+        Me.btnLinSearch.Location = New System.Drawing.Point(176, 181)
+        Me.btnLinSearch.Name = "btnLinSearch"
+        Me.btnLinSearch.Size = New System.Drawing.Size(118, 88)
+        Me.btnLinSearch.TabIndex = 0
+        Me.btnLinSearch.Text = "Start Linear Search"
+        Me.btnLinSearch.UseVisualStyleBackColor = True
+        '
+        'btn2DArray
+        '
+        Me.btn2DArray.Location = New System.Drawing.Point(440, 42)
+        Me.btn2DArray.Name = "btn2DArray"
+        Me.btn2DArray.Size = New System.Drawing.Size(130, 29)
+        Me.btn2DArray.TabIndex = 1
+        Me.btn2DArray.Text = "2D Array Build"
+        Me.btn2DArray.UseVisualStyleBackColor = True
+        '
+        'btnRow
+        '
+        Me.btnRow.Location = New System.Drawing.Point(369, 77)
+        Me.btnRow.Name = "btnRow"
+        Me.btnRow.Size = New System.Drawing.Size(75, 23)
+        Me.btnRow.TabIndex = 2
+        Me.btnRow.Text = "Row Output"
+        Me.btnRow.UseVisualStyleBackColor = True
+        '
+        'btnColumn
+        '
+        Me.btnColumn.Location = New System.Drawing.Point(462, 77)
+        Me.btnColumn.Name = "btnColumn"
+        Me.btnColumn.Size = New System.Drawing.Size(92, 23)
+        Me.btnColumn.TabIndex = 3
+        Me.btnColumn.Text = "Column Output"
+        Me.btnColumn.UseVisualStyleBackColor = True
+        '
+        'btnFullArray
+        '
+        Me.btnFullArray.Location = New System.Drawing.Point(369, 107)
+        Me.btnFullArray.Name = "btnFullArray"
+        Me.btnFullArray.Size = New System.Drawing.Size(75, 23)
+        Me.btnFullArray.TabIndex = 4
+        Me.btnFullArray.Text = "Full Array"
+        Me.btnFullArray.UseVisualStyleBackColor = True
+        '
+        'btnSearch2D
+        '
+        Me.btnSearch2D.Location = New System.Drawing.Point(462, 107)
+        Me.btnSearch2D.Name = "btnSearch2D"
+        Me.btnSearch2D.Size = New System.Drawing.Size(92, 23)
+        Me.btnSearch2D.TabIndex = 5
+        Me.btnSearch2D.Text = "Search Array"
+        Me.btnSearch2D.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnSearch2D)
+        Me.Controls.Add(Me.btnFullArray)
+        Me.Controls.Add(Me.btnColumn)
+        Me.Controls.Add(Me.btnRow)
+        Me.Controls.Add(Me.btn2DArray)
+        Me.Controls.Add(Me.btnLinSearch)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnLinSearch As Button
+    Friend WithEvents btn2DArray As Button
+    Friend WithEvents btnRow As Button
+    Friend WithEvents btnColumn As Button
+    Friend WithEvents btnFullArray As Button
+    Friend WithEvents btnSearch2D As Button
+End Class
---
+++ BasicSearches/Form1.vb
@@ -0,0 +1,173 @@
+﻿Public Class Form1
+    Private Sub btnLinSearch_Click(sender As Object, e As EventArgs) Handles btnLinSearch.Click
+
+        Dim stTarget As String
+        Dim astItems(9) As String
+
+        astItems(0) = "Disc"
+        astItems(1) = "Flange"
+        astItems(2) = "Wheel"
+        astItems(3) = "Bit"
+        astItems(4) = "Bolt"
+        astItems(5) = "Nut"
+        astItems(6) = "Hose"
+        astItems(7) = "Valve"
+        astItems(8) = "Chuck"
+        astItems(9) = "Compressor"
+
+        stTarget = InputBox("Enter item to search inventory: ")
+
+        Dim i As Integer
+        For i = 0 To 9
+            If UCase(astItems(i)) = UCase(stTarget) Then
+                MessageBox.Show("Item: " & """" & astItems(i) & """" & " is in stock." & vbNewLine _
+                                & "Item is in store rack #: " & i)
+                Exit For
+            End If
+        Next
+    End Sub
+
+    Private Sub btn2DArray_Click(sender As Object, e As EventArgs) Handles btn2DArray.Click
+
+        Dim astDescriptions(3, 3) As String
+        Dim i As Integer
+        astDescriptions(0, 0) = "Software"
+        astDescriptions(1, 0) = "Developer"
+        astDescriptions(2, 0) = "Me"
+
+        astDescriptions(0, 1) = "Program"
+        astDescriptions(1, 1) = "Writer"
+        astDescriptions(2, 1) = "You"
+
+        astDescriptions(0, 2) = "Computer"
+        astDescriptions(1, 2) = "Friends"
+        astDescriptions(2, 2) = "Us"
+
+        For i = 0 To 2
+            MessageBox.Show(astDescriptions(i, 1))
+        Next
+
+    End Sub
+
+    Private Sub btnRow_Click(sender As Object, e As EventArgs) Handles btnRow.Click
+
+        Dim astDescriptions(3, 3) As String
+
+        astDescriptions(0, 0) = "Software"
+        astDescriptions(1, 0) = "Developer"
+        astDescriptions(2, 0) = "Me"
+
+        astDescriptions(0, 1) = "Program"
+        astDescriptions(1, 1) = "Writer"
+        astDescriptions(2, 1) = "You"
+
+        astDescriptions(0, 2) = "Computer"
+        astDescriptions(1, 2) = "Friends"
+        astDescriptions(2, 2) = "Us"
+
+        Dim x As Integer
+        Dim y As Integer
+
+        For y = 0 To 2
+            For x = 0 To 2
+                MessageBox.Show(astDescriptions(x, y))
+            Next
+        Next
+    End Sub
+
+    Private Sub btnColumn_Click(sender As Object, e As EventArgs) Handles btnColumn.Click
+        Dim astDescriptions(3, 3) As String
+
+        astDescriptions(0, 0) = "Software"
+        astDescriptions(1, 0) = "Developer"
+        astDescriptions(2, 0) = "Me"
+
+        astDescriptions(0, 1) = "Program"
+        astDescriptions(1, 1) = "Writer"
+        astDescriptions(2, 1) = "You"
+
+        astDescriptions(0, 2) = "Computer"
+        astDescriptions(1, 2) = "Friends"
+        astDescriptions(2, 2) = "Us"
+
+        Dim x As Integer
+        Dim y As Integer
+
+        For x = 0 To 2
+            For y = 0 To 2
+                MessageBox.Show(astDescriptions(x, y))
+            Next
+        Next
+    End Sub
+
+    Private Sub btnFullArray_Click(sender As Object, e As EventArgs) Handles btnFullArray.Click
+        Dim astDescriptions(3, 3) As String
+
+        astDescriptions(0, 0) = "Software"
+        astDescriptions(1, 0) = "Developer"
+        astDescriptions(2, 0) = "Me"
+
+        astDescriptions(0, 1) = "Program"
+        astDescriptions(1, 1) = "Writer"
+        astDescriptions(2, 1) = "You"
+
+        astDescriptions(0, 2) = "Computer"
+        astDescriptions(1, 2) = "Friends"
+        astDescriptions(2, 2) = "Us"
+
+        Dim x As Integer
+        Dim y As Integer
+        Dim stOut As String
+
+        For y = 0 To 2
+            For x = 0 To 2
+                stOut = stOut & " " & astDescriptions(x, y)
+            Next
+            stOut = stOut & vbNewLine
+        Next
+        MessageBox.Show(stOut)
+    End Sub
+
+    Private Sub btnSearch2D_Click(sender As Object, e As EventArgs) Handles btnSearch2D.Click
+        Dim astDescriptions(3, 3) As String
+
+        astDescriptions(0, 0) = "Software"
+        astDescriptions(1, 0) = "Developer"
+        astDescriptions(2, 0) = "Me"
+
+        astDescriptions(0, 1) = "Program"
+        astDescriptions(1, 1) = "Writer"
+        astDescriptions(2, 1) = "You"
+
+        astDescriptions(0, 2) = "Computer"
+        astDescriptions(1, 2) = "Friends"
+        astDescriptions(2, 2) = "Us"
+
+        Dim x As Integer
+        Dim y As Integer
+        Dim bIsFound As Boolean
+        Dim stTarget As String
+        Dim stOut As String
+
+        bIsFound = False
+
+        stTarget = InputBox("Enter your search target: ")
+
+        For y = 0 To 2
+            If astDescriptions(0, y) = stTarget Then
+                bIsFound = True
+                Exit For
+
+            End If
+        Next
+
+        If bIsFound = True Then
+            For x = 0 To 2
+                stOut = stOut & astDescriptions(x, y) & " "
+            Next
+        Else
+            MessageBox.Show("Not found...")
+        End If
+        MessageBox.Show(stOut)
+    End Sub
+End Class
